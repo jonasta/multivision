8f009fb0ada53b30ecff3b292201c40918973dae
Jonasta <jonastads@gmail.com>
2017-10-04 16:22:05 -0300

    added ui, separated main ctrl

--- server.js
+++ server.js
@@ -35,15 +35,6 @@ db.on('error', console.error.bind(console, 'mongodb : connection error...'));
 db.on('open', function () {
   console.log('mongodb : database connected');
 });
-//message schema setup
-var messageSchema = mongoose.Schema({
-  message: String
-});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function (err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
 
 //angular partials setup
 app.get('/partials/:partialPath', function (req, res) {
@@ -61,9 +52,7 @@ app.set('view engine', 'jade');
 
 app.get('*', function (req, res) {
   console.log('render index');
-  res.render('index', {
-    mongoMessage : mongoMessage
-  });
+  res.render('index');
 });
 
 var port = process.env.PORT || 3030;
