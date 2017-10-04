54ab8332662371e1cfe581ad5ed710c190f0b7e2
Jonasta <jonastads@gmail.com>
2017-10-04 10:51:55 -0300

    mongodb added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV | 'development';
 var app = express();
@@ -20,6 +22,17 @@ app.use(stylus.middleware(
 
 //static route to /public
 app.use(express.static(__dirname + '/public'));
+
+//mongodb setup
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb : connection error...'));
+db.on('open', function(){
+  console.log('mongodb : database connected');
+});
+
+
+//angular partials setup
 app.get('/partials/:partialPath', function (req, res) {
   console.log('partials/' + req.params.partialPath);
   res.render('partials/' + req.params.partialPath)
@@ -29,7 +42,6 @@ app.get('/partials/:partialPath', function (req, res) {
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
